7b5495dc108056ff5ce6f57fcfd64c7b0f1f9236
mohamed mhiri <mhiri.mohamed.5@gmail.com>
2020-04-22 12:32:35 +0100

    starting with the 1st context: money and snackmachine

--- domain-driven-design-in-practice/starting-with-the-first-bounded-context/DddInPractice.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DddInPractice.Logic
-{
-    public class Class1
-    {
-    }
-}
---
+++ domain-driven-design-in-practice/starting-with-the-first-bounded-context/DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DddInPractice.Logic
+{
+  public sealed class SnackMachine
+  {
+    public Money MoneyInside { get; private set; }
+    public Money MoneyInTransaction { get; private set; }
+
+    public void InsertMoney(
+        Money money
+    )
+    {
+      MoneyInTransaction = money;
+    }
+
+    public void ReturnMoney()
+    {
+      // MoneyInTransaction = new Money();
+    }
+
+    public void BuySnack()
+    {
+      MoneyInside += MoneyInTransaction;
+      // MoneyInTransaction = new Money();
+    }
+
+  }
+}
