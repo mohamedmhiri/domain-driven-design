1fe016be59b8ba87dc5471d3da09723b8c49e12c
mohamed mhiri <mhiri.mohamed.5@gmail.com>
2020-04-25 17:28:49 +0100

    - tunisian coins - substraction - Amount property - test substraction - test amount count

--- domain-driven-design-in-practice/starting-with-the-first-bounded-context/DddInPractice.Logic/Money.cs
+++ domain-driven-design-in-practice/starting-with-the-first-bounded-context/DddInPractice.Logic/Money.cs
@@ -1,64 +1,98 @@
+using System;
+
 namespace DddInPractice.Logic
 {
   public sealed class Money : ValueObject<Money>
   {
-    public int OneCentCount { get; private set; }
-    public int TenCentCount { get; private set; }
-    public int QuarterCount { get; private set; }
-    public int OneDollarCount { get; private set; }
-    public int FiveDollarCount { get; private set; }
-    public int TwentyDollarCount { get; private set; }
+    public int TenMillimesCount { get; private set; }
+    public int TwentyMillimesCount { get; private set; }
+    public int FiftyMillimesCount { get; private set; }
+    public int OneHundredMillimesCount { get; private set; }
+    public int TwoHundredMillimesCount { get; private set; }
+    public int FiveHundredMillimesCount { get; private set; }
+
+    public decimal Amount => 
+        TenMillimesCount * 0.01m +
+        TwentyMillimesCount * 0.02m +
+        FiftyMillimesCount * 0.05m +
+        OneHundredMillimesCount * 0.1m +
+        TwoHundredMillimesCount * 0.2m +
+        FiveHundredMillimesCount * 0.5m;
 
     public Money(
-      int oneCentCount,
-      int tenCentCount,
-      int quarterCount,
-      int oneDollarCount,
-      int fiveDollarCount,
-      int twentyDollarCount
+      int tenMillimesCount,
+      int twentyMillimesCount,
+      int fiftyMillimesCount,
+      int oneHundredMillimesCount,
+      int twoHundredMillimesCount,
+      int fiveHundredMillimesCount
     )
     {
-      OneCentCount = oneCentCount;
-      TenCentCount = tenCentCount;
-      QuarterCount = quarterCount;
-      OneDollarCount = oneDollarCount;
-      FiveDollarCount = fiveDollarCount;
-      TwentyDollarCount = twentyDollarCount;
+      if (tenMillimesCount < 0)
+        throw new InvalidOperationException();
+      if (twentyMillimesCount < 0)
+        throw new InvalidOperationException();
+      if (fiftyMillimesCount < 0)
+        throw new InvalidOperationException();
+      if (oneHundredMillimesCount < 0)
+        throw new InvalidOperationException();
+      if (twoHundredMillimesCount < 0)
+        throw new InvalidOperationException();
+      if (fiveHundredMillimesCount < 0)
+        throw new InvalidOperationException();
+      TenMillimesCount = tenMillimesCount;
+      TwentyMillimesCount = twentyMillimesCount;
+      FiftyMillimesCount = fiftyMillimesCount;
+      OneHundredMillimesCount = oneHundredMillimesCount;
+      TwoHundredMillimesCount = twoHundredMillimesCount;
+      FiveHundredMillimesCount = fiveHundredMillimesCount;
     }
 
     public static Money operator +(Money money1, Money money2)
     {
       Money sum = new Money(
-        money1.OneCentCount + money2.OneCentCount,
-        money1.TenCentCount + money2.TenCentCount,
-        money1.QuarterCount + money2.QuarterCount,
-        money1.OneDollarCount + money2.OneDollarCount,
-        money1.FiveDollarCount + money2.FiveDollarCount,
-        money1.TwentyDollarCount + money2.TwentyDollarCount
+        money1.TenMillimesCount + money2.TenMillimesCount,
+        money1.TwentyMillimesCount + money2.TwentyMillimesCount,
+        money1.FiftyMillimesCount + money2.FiftyMillimesCount,
+        money1.OneHundredMillimesCount + money2.OneHundredMillimesCount,
+        money1.TwoHundredMillimesCount + money2.TwoHundredMillimesCount,
+        money1.FiveHundredMillimesCount + money2.FiveHundredMillimesCount
+      );
+      return sum;
+    }
+    public static Money operator -(Money money1, Money money2)
+    {
+      Money sum = new Money(
+        money1.TenMillimesCount - money2.TenMillimesCount,
+        money1.TwentyMillimesCount - money2.TwentyMillimesCount,
+        money1.FiftyMillimesCount - money2.FiftyMillimesCount,
+        money1.OneHundredMillimesCount - money2.OneHundredMillimesCount,
+        money1.TwoHundredMillimesCount - money2.TwoHundredMillimesCount,
+        money1.FiveHundredMillimesCount - money2.FiveHundredMillimesCount
       );
       return sum;
     }
 
     protected override bool EqualsCore(Money other)
     {
-      return OneCentCount == other.OneCentCount
-          && TenCentCount == other.TenCentCount
-          && QuarterCount == other.QuarterCount
-          && OneDollarCount == other.OneDollarCount
-          && FiveDollarCount == other.FiveDollarCount
-          && TwentyDollarCount == other.TwentyDollarCount;
+      return TenMillimesCount == other.TenMillimesCount
+          && TwentyMillimesCount == other.TwentyMillimesCount
+          && FiftyMillimesCount == other.FiftyMillimesCount
+          && OneHundredMillimesCount == other.OneHundredMillimesCount
+          && TwoHundredMillimesCount == other.TwoHundredMillimesCount
+          && FiveHundredMillimesCount == other.FiveHundredMillimesCount;
     }
 
     protected override int GetHashCodeCore()
     {
       unchecked
       {
-        int hashCode = OneCentCount;
-        hashCode = (hashCode * 397) ^ TenCentCount;
-        hashCode = (hashCode * 397) ^ QuarterCount;
-        hashCode = (hashCode * 397) ^ OneDollarCount;
-        hashCode = (hashCode * 397) ^ FiveDollarCount;
-        hashCode = (hashCode * 397) ^ TwentyDollarCount;
+        int hashCode = TenMillimesCount;
+        hashCode = (hashCode * 397) ^ TwentyMillimesCount;
+        hashCode = (hashCode * 397) ^ FiftyMillimesCount;
+        hashCode = (hashCode * 397) ^ OneHundredMillimesCount;
+        hashCode = (hashCode * 397) ^ TwoHundredMillimesCount;
+        hashCode = (hashCode * 397) ^ FiveHundredMillimesCount;
         return hashCode;
       }
     }
